7bfeff4234e1522b6cfdc79a44aec52d72cb0ab3
N2st2s1a <anastasiaborisenko53@gmail.com>
2023-11-03 17:09:23 +0100

    fragments_5pscs

--- app/src/main/java/com/example/poms_city_transport/MainActivity.java
+++ app/src/main/java/com/example/poms_city_transport/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.poms_city_transport;
 
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.poms_city_transport.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,15 +23,45 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Заменяем фрагмент по умолчанию (может потребоваться импортировать фрагменты)
+        replaceFragment(new HomeFragment());
+
+        BottomNavigationView navView = binding.navView; // Исправляем название переменной
+
+        navView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case 0: // Для R.id.navigation_home
+                    replaceFragment(new HomeFragment());
+                    break;
+                case 1: // Для R.id.navigation_map
+                    replaceFragment(new mapFragment());
+                    break;
+                case 2: // Для R.id.navigation_route
+                    replaceFragment(new routeFragment());
+                    break;
+                case 3: // Для R.id.navigation_bookmarks
+                    replaceFragment(new bookmarksFragment());
+                    break;
+                case 4: // Для R.id.navigation_info
+                    replaceFragment(new infoFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void setupNavigation() {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_adress, R.id.navigation_info,R.id.navigation_routes,R.id.navigation_bookmarks)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
